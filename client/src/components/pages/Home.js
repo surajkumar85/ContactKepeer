c1cfbcde8be99a177aca8e1dda4a0491e41d5c93
suman276 <61776255+suman276@users.noreply.github.com>
2021-10-22 22:36:31 +0530

    Update Home.js

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -5,7 +5,22 @@ import ContactFilter from "../contacts/ContactFilter"
 import AuthContext from "../../context/auth/authContext"
 const  Home= ()=> {
     const authContext = useContext(AuthContext)
-   
+    useEffect(()=>{
+        authContext.loadUser();
+        //eslint-disable-next-line
+    },[])
+    return (
+        <div className="grid-2">
+            <div>
+                <ContactForm />
+            </div>
+            <div>
+                <ContactFilter />
+                <Contacts />
+            </div>
+        </div>
+    )
+
 }
 
 export default Home
